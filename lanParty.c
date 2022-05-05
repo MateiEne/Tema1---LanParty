81fe310512f4538cdebf4ff1945208d72b9b7b0f
MateiEne <ene.matei21@gmail.com>
2022-05-05 21:56:25 +0300

    added function to eliminate teams but encountered a few bugs. Need to fix that

--- lanParty.c
+++ lanParty.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define INT_MAX 10000
+
 typedef struct Player {
     char* firstName;
     char* secondName;
@@ -33,23 +35,41 @@ float calculateTeamPoints(Player* players, int nrOfPlayers) {
     return (p / nrOfPlayers);
 }
 
+int calculateNrOfTeams(char fileName[]) {
+    FILE* f = fopen(fileName, "rt");
+
+    if (f == NULL) {
+        printf("cannot open the file\n");
+        exit(-1);
+    }
+
+    int nrOfTeams;
+    fscanf(f, "%d", &nrOfTeams);
+
+    fclose(f);
+
+    return nrOfTeams;
+}
+
 Team getTeam(FILE* f) {
     Team team;
 
     fscanf(f, "%d ", &team.nrPlayers);
 
     team.players = (Player*)malloc(team.nrPlayers * sizeof(Player));
-    
+
     char auxTeamName[56];
 
+    int i;
+
     fgets(auxTeamName, 56, f);
-    team.name = (char*)malloc((strlen(auxTeamName) + 1) * sizeof(char));
+    auxTeamName[strlen(auxTeamName) - 1] = '\0';
+    team.name = (char*)malloc((strlen(auxTeamName)) * sizeof(char));
     strcpy(team.name, auxTeamName);
 
     char auxPlayerFirstName[26];
     char auxPlayerSecondName[26];
 
-    int i;
     for (i = 0; i < team.nrPlayers; i++) {
         fscanf(f, "%s", auxPlayerFirstName);
         team.players[i].firstName = (char*)malloc((strlen(auxPlayerFirstName) + 1) * sizeof(char));
@@ -127,7 +147,7 @@ void printTeams(Node* head) {
         return;
     }
 
-    printf("%s %f\n", head->team.name, head->team.points);
+    printf("%s\n", head->team.name);
     printTeams(head->next);
 }
 
@@ -135,24 +155,82 @@ int calculateNumberOfTeamsToEliminate(int nrOfTeams) {
     int i = 2;
 
     while (i * 2 <= nrOfTeams) {
-        i *= 2;
+        i *= 2; 
     }
 
     return nrOfTeams - i;
 }
 
 Node* findPrevTeamOfMinPointsTeam(Node* head) {
-    
+    if (head == NULL) {
+        return NULL;
+    }
+
+    if (head->next == NULL) {
+        return head;
+    }
+
+    int minTeamPoints = INT_MAX;
+
+    Node* currentNode = head;
+    Node* prevNodeToCurrentNode = head;
+    Node* prevNodeToMin;
+
+    while (currentNode) {
+        if (currentNode->team.points < minTeamPoints) {
+            minTeamPoints = head->team.points;
+            prevNodeToMin = prevNodeToCurrentNode;
+        }
+
+        prevNodeToCurrentNode = currentNode;
+        currentNode = currentNode->next;
+    }
+
+    return prevNodeToMin;
+}
+
+void eliminateTeamFromList(Node* node, Node** head) {
+    if (node == *head) {
+        *head = (*head)->next; 
+        free(node);
+
+        return;
+    }
+
+    Node* nodeToEliminate = node->next;
+    node->next = nodeToEliminate->next;
+
+    free(nodeToEliminate);
+}
+
+void eliminateTeams(Node** head, int* nrOfTeams) {
+    int nrOfTeamsToEliminate = calculateNumberOfTeamsToEliminate(*nrOfTeams);
+
+    *nrOfTeams -= nrOfTeamsToEliminate;
+
+    int i;
+
+    Node* prevNodeToMinTeamPoints;
+
+    for (i = 0; i < nrOfTeamsToEliminate; i++) {
+        prevNodeToMinTeamPoints = findPrevTeamOfMinPointsTeam(*head);
+        printf("%c\n", prevNodeToMinTeamPoints->next->team.name[strlen(prevNodeToMinTeamPoints->next->team.name) - 2]);
+        printf("%s \n", prevNodeToMinTeamPoints->next->team.name);
+        eliminateTeamFromList(prevNodeToMinTeamPoints, &(*head));
+    }
 }
 
 int main() {
     Node* head = NULL;
 
     createTeamsList("date/t1/d.in", &head);
+    
+    int nrOfTeams = calculateNrOfTeams("date/t1/d.in");
+
+    eliminateTeams(&head, &nrOfTeams);
 
     //printTeams(head);
-
-    int n = calculateNumberOfTeamsToEliminate(146);
-
-    printf("%d\n", n);
 }
+
+// TO DO: at the end of every team name, there was an \n and I replaced that with \0 but still something is wrong. Fix that!
+// TO DO: the teams are not elimineated corected (not by minimum points)
